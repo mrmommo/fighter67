b7f058a5c81a505cd8c3a9bacc69beb92c7391e5
iamnpc-huy <huy.da04042006@gmail.com>
2026-06-04 10:03:24 +0700

    Revert "nothing"
    This reverts commit ff1a015e1196adbda01e40bb2c75a816fe9d9a73.

--- src/main/java/com/nhom67/platformfighter/entity/EntityFactory.java
+++ src/main/java/com/nhom67/platformfighter/entity/EntityFactory.java
@@ -18,85 +18,83 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class EntityFactory implements com.almasb.fxgl.entity.EntityFactory {
-        public static final short CATEGORY_GROUND = 0x0001; // Sàn cứng
-        public static final short CATEGORY_ONE_WAY = 0x0002; // Sàn mềm
-        public static final short CATEGORY_PLAYER = 0x0004; // Người chơi
+    public static final short CATEGORY_GROUND = 0x0001;   // Sàn cứng
+    public static final short CATEGORY_ONE_WAY = 0x0002;  // Sàn mềm
+    public static final short CATEGORY_PLAYER = 0x0004;   // Người chơi
 
-        @Spawns("platform")
-        public Entity newPlatform(SpawnData data) {
-                PhysicsComponent physics = new PhysicsComponent();
-                physics.setBodyType(BodyType.STATIC);
+    @Spawns("platform")
+    public Entity newPlatform(SpawnData data) {
+        PhysicsComponent physics = new PhysicsComponent();
+        physics.setBodyType(BodyType.STATIC);
 
-                // Gán thể loại là SÀN CỨNG
-                FixtureDef fd = new FixtureDef();
-                fd.getFilter().categoryBits = CATEGORY_GROUND;
-                physics.setFixtureDef(fd);
+        // Gán thể loại là SÀN CỨNG
+        FixtureDef fd = new FixtureDef();
+        fd.getFilter().categoryBits = CATEGORY_GROUND;
+        physics.setFixtureDef(fd);
 
-                return entityBuilder(data)
-                                .type(EntityType.PLATFORM)
-                                .bbox(new HitBox(BoundingShape.box(((Number) data.get("width")).doubleValue(),
-                                                ((Number) data.get("height")).doubleValue())))
-                                .with(physics)
-                                .build();
-        }
+        return entityBuilder(data)
+                .type(EntityType.PLATFORM)
+                .bbox(new HitBox(BoundingShape.box(((Number) data.get("width")).doubleValue(), ((Number) data.get("height")).doubleValue())))
+                .with(physics)
+                .build();
+    }
 
-        @Spawns("oneWayPlatform") // Dùng cái này cho các bục nhảy trên không
-        public Entity newOneWayPlatform(SpawnData data) {
-                PhysicsComponent physics = new PhysicsComponent();
-                physics.setBodyType(BodyType.STATIC);
+    @Spawns("oneWayPlatform") // Dùng cái này cho các bục nhảy trên không
+    public Entity newOneWayPlatform(SpawnData data) {
+        PhysicsComponent physics = new PhysicsComponent();
+        physics.setBodyType(BodyType.STATIC);
 
-                // Gán thể loại là SÀN MỀM
-                FixtureDef fd = new FixtureDef();
-                fd.getFilter().categoryBits = CATEGORY_ONE_WAY;
-                physics.setFixtureDef(fd);
+        // Gán thể loại là SÀN MỀM
+        FixtureDef fd = new FixtureDef();
+        fd.getFilter().categoryBits = CATEGORY_ONE_WAY;
+        physics.setFixtureDef(fd);
 
-                return entityBuilder(data)
-                                .type(EntityType.ONE_WAY_PLATFORM) // Yêu cầu thêm vào enum EntityType
-                                .bbox(new HitBox(BoundingShape.box(((Number) data.get("width")).doubleValue(),
-                                                ((Number) data.get("height")).doubleValue())))
-                                .with(physics)
-                                .build();
-        }
+        return entityBuilder(data)
+                .type(EntityType.ONE_WAY_PLATFORM) // Yêu cầu thêm vào enum EntityType
+                .bbox(new HitBox(BoundingShape.box(((Number) data.get("width")).doubleValue(), ((Number) data.get("height")).doubleValue())))
+                .with(physics)
+                .build();
+    }
 
-        @Spawns("player")
-        public Entity newPlayer(SpawnData data) {
-                PhysicsComponent physics = new PhysicsComponent();
-                physics.setBodyType(BodyType.DYNAMIC);
-                physics.addGroundSensor(new HitBox("GROUND_SENSOR", new Point2D(27, 58), BoundingShape.box(6, 8)));
+    @Spawns("player")
+    public Entity newPlayer(SpawnData data) {
+        PhysicsComponent physics = new PhysicsComponent();
+        physics.setBodyType(BodyType.DYNAMIC);
+        physics.addGroundSensor(new HitBox("GROUND_SENSOR", new Point2D(27, 58), BoundingShape.box(6, 8)));
 
-                // Thiết lập bộ lọc mặc định cho Player
-                FixtureDef fd = new FixtureDef();
-                fd.setFriction(0.0f);
-                fd.getFilter().categoryBits = CATEGORY_PLAYER;
-                // Mặc định: Chạm vào CẢ Sàn cứng VÀ Sàn mềm
-                fd.getFilter().maskBits = CATEGORY_GROUND | CATEGORY_ONE_WAY;
-                physics.setFixtureDef(fd);
+        // Thiết lập bộ lọc mặc định cho Player
+        FixtureDef fd = new FixtureDef();
+        fd.setFriction(0.0f);
+        fd.getFilter().categoryBits = CATEGORY_PLAYER;
+        // Mặc định: Chạm vào CẢ Sàn cứng VÀ Sàn mềm
+        fd.getFilter().maskBits = CATEGORY_GROUND | CATEGORY_ONE_WAY;
+        physics.setFixtureDef(fd);
 
-                Color color = data.hasKey("color") ? data.get("color") : Color.GREEN;
-                String prefix = color.equals(Color.GREEN) ? "p1" : "p2";
+        Color color = data.hasKey("color") ? data.get("color") : Color.GREEN;
+        String prefix = color.equals(Color.GREEN) ? "p1" : "p2";
 
-                return entityBuilder(data)
-                                .type(EntityType.PLAYER)
-                                .bbox(new HitBox(new Point2D(22, 11), BoundingShape.circle(8)))
-                                .bbox(new HitBox(new Point2D(22, 27), BoundingShape.box(16, 32)))
-                                .with(physics)
-                                .with(new CollidableComponent(true))
-                                .with(new IrremovableComponent())
-                                .with(new PlayerComponent())
-                                .with(new AnimationComponent(prefix))
-                                .build();
-        }
+        return entityBuilder(data)
+                .type(EntityType.PLAYER)
+                .bbox(new HitBox(new Point2D(22, 11), BoundingShape.circle(8)))
+                .bbox(new HitBox(new Point2D(22, 27), BoundingShape.box(16, 32)))
+                .with(physics)
+                .with(new CollidableComponent(true))
+                .with(new IrremovableComponent())
+                .with(new PlayerComponent())
+                .with(new AnimationComponent(prefix))
+                .build();
+    }
 
-        @Spawns("KillZone")
-        public Entity newKillZone(SpawnData data) {
-                return entityBuilder(data)
-                                .type(EntityType.KILL_ZONE)
-                                .bbox(new HitBox(BoundingShape.box(((Number) data.get("width")).doubleValue(),
-                                                ((Number) data.get("height")).doubleValue())))
-                                .with(new CollidableComponent(true)) // Sử dụng CollidableComponent thay cho
-                                                                     // PhysicsComponent để không
-                                                                     // lỗi Sensor handler
-                                .build();
-        }
+    @Spawns("KillZone")
+    public Entity newKillZone(SpawnData data) {
+        return entityBuilder(data)
+                .type(EntityType.KILL_ZONE)
+                .bbox(new HitBox(BoundingShape.box(((Number) data.get("width")).doubleValue(),
+                        ((Number) data.get("height")).doubleValue())))
+                .with(new CollidableComponent(true)) // Sử dụng CollidableComponent thay cho PhysicsComponent để không
+                                                     // lỗi Sensor handler
+                .build();
+    }
+
 
 }
