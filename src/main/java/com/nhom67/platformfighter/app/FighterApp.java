fb847d48474910a39c19f4e4811a6d0ee30b3bae
Vua Dev <long16102006@gmail.com>
2026-06-07 10:17:31 +0700

    player drop

--- src/main/java/com/nhom67/platformfighter/app/FighterApp.java
+++ src/main/java/com/nhom67/platformfighter/app/FighterApp.java
@@ -93,8 +93,8 @@ public class FighterApp extends GameApplication {
         }
 
         //
-        player = spawn("player", new SpawnData(400, 0).put("color", Color.GREEN));
-        player2 = spawn("player", new SpawnData(1520, 0).put("color", Color.BLUE));
+        player = spawn("player", new SpawnData(400, -2000).put("color", Color.GREEN));
+        player2 = spawn("player", new SpawnData(1520, -2000).put("color", Color.BLUE));
 
         boolean vsBot = gameMode == GameMode.VS_BOT;
         if (vsBot) {
